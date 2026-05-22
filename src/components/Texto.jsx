cded51aaf32bdc7e04ef4b19db9bd7e13f5f0474
Priscila <pyea08@gmail.com>
2026-05-22 10:38:41 -0300

    feat propriedades texto generico

---
+++ src/components/Texto.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Paragrafo = styled.p`
+  text-align: justify;
+  font-size: 1.15rem;
+  line-height: 1.6;
+  margin-bottom: 15px;
+  color: ${(props) => props.theme.text};
+  margin-bottom: 20px;
+
+  span {
+    color: ${(props) => props.theme.highlight};
+    font-weight: bold;
+  }
+
+  ${(props) =>
+    props.destaque &&
+    `color: ${props.theme.highlight};
+       font-weight: bold;
+       font-size: 1.3rem;
+       margin-bottom: 25px;`}
+`;
+
+function Texto({ children, $destaque, ...props }) {
+  return (
+    <Paragrafo {...props} $destaque={$destaque}>
+      {children}
+    </Paragrafo>
+  );
+}
+export default Texto;
